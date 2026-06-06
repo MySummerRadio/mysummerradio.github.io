8f60d7aef4563ff5a12b2b5bcaf3530aa390f7c5
Olivier Cardinaux <github@ocx.aleeas.com>
2026-06-06 06:40:51 +0200

    feat: add astro config image objectfit

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,4 +4,9 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://mysummerradio.github.io",
+  image: {
+    layout: "constrained",
+    objectFit: "contain",
+    objectPosition: "center",
+  },
 });
