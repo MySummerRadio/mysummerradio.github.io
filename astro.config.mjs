07b9a3def3339fde8258c0c47f03210552281e68
Olivier Cardinaux <github@ocx.aleeas.com>
2026-07-16 13:56:34 +0200

    chore: add CNAME for staging repo

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mysummerradio.github.io",
+  site: "https://staging.mysummerradio.com",
   image: {
     layout: "constrained",
     objectFit: "contain",
