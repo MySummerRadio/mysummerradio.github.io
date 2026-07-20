7017c0cb8b9497b3f257c91f72476bbb9dd1fc73
Olivier Cardinaux <github@ocx.aleeas.com>
2026-07-20 14:59:20 +0200

    feat: add rss feed to blog

---
+++ src/pages/feed.xml.js
@@ -0,0 +1,25 @@
+import rss from '@astrojs/rss';
+import { getCollection } from 'astro:content';
+
+export async function GET(context) {
+  const blog = await getCollection('blog');
+  return rss({
+    // `<title>` field in output xml
+    title: "My Summer Radio's Blog",
+    // `<description>` field in output xml
+    description: 'My Summer Radio is a music production blog',
+    // Pull in your project "site" from the endpoint context
+    // https://docs.astro.build/en/reference/api-reference/#site
+    site: context.site,
+    // Array of `<item>`s in output xml
+    // See "Generating items" section for examples using content collections and glob imports
+    items: blog.map((post) => ({
+      title: post.data.title,
+      description: post.data.description,
+      pubDate: post.data.pubDate,
+      link: `/blog/${post.id}`,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  });
+}
