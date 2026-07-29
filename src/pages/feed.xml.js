179f3a7304603aae50020698e9deaf8955077905
Olivier Cardinaux <github@ocx.aleeas.com>
2026-07-29 10:43:23 +0200

    chore: update dependencies

--- src/pages/feed.xml.js
+++ src/pages/feed.xml.js
@@ -7,7 +7,7 @@ export async function GET(context) {
     // `<title>` field in output xml
     title: "My Summer Radio's Blog",
     // `<description>` field in output xml
-    description: 'My Summer Radio is a music production blog',
+    description: 'My Summer Radio is a musician\'s  blog',
     // Pull in your project "site" from the endpoint context
     // https://docs.astro.build/en/reference/api-reference/#site
     site: context.site,
